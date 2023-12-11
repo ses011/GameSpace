fba02dd1487eaf073b59c5ef2eaf55acc1fe7e20
Kash <kasha@DESKTOP-TKSG659>
2023-12-11 13:11:53 -0500

    Edited

--- GameChat/Form1.cs
+++ GameChat/Form1.cs
@@ -8,11 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//add using statements here for the player info database and the profile-display class 
+//so the error about the user object is gone
+
 namespace GameChat
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1(Form form, User user)
         {
             InitializeComponent();
         }

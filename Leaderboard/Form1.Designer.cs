6aa571801cbe0bca5b1a192e264c877292148ab6
Autumn ☆ <autumnderhammer2@gmail.com>
2023-12-13 10:49:00 -0500

    leaderboard commit

--- Leaderboard/Form1.Designer.cs
+++ Leaderboard/Form1.Designer.cs
@@ -28,110 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.overallButton = new System.Windows.Forms.RadioButton();
-            this.timeButton = new System.Windows.Forms.RadioButton();
-            this.scoreButton = new System.Windows.Forms.RadioButton();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.Rank = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Username = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Value = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.leaderboard = new System.Windows.Forms.ListView();
             this.label1 = new System.Windows.Forms.Label();
             this.gameGroupBox = new System.Windows.Forms.GroupBox();
+            this.gameRadioButton6 = new System.Windows.Forms.RadioButton();
+            this.gameRadioButton5 = new System.Windows.Forms.RadioButton();
+            this.gameRadioButton4 = new System.Windows.Forms.RadioButton();
             this.gameRadioButton3 = new System.Windows.Forms.RadioButton();
             this.gameRadioButton2 = new System.Windows.Forms.RadioButton();
             this.gameRadioButton1 = new System.Windows.Forms.RadioButton();
-            this.gameRadioButton4 = new System.Windows.Forms.RadioButton();
-            this.gameRadioButton5 = new System.Windows.Forms.RadioButton();
-            this.gameRadioButton6 = new System.Windows.Forms.RadioButton();
             this.label2 = new System.Windows.Forms.Label();
-            this.groupBox1.SuspendLayout();
             this.gameGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // leaderboard
             // 
-            this.groupBox1.Controls.Add(this.scoreButton);
-            this.groupBox1.Controls.Add(this.timeButton);
-            this.groupBox1.Controls.Add(this.overallButton);
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(542, 75);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 147);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Filter";
-            // 
-            // overallButton
-            // 
-            this.overallButton.AutoSize = true;
-            this.overallButton.Location = new System.Drawing.Point(18, 29);
-            this.overallButton.Name = "overallButton";
-            this.overallButton.Size = new System.Drawing.Size(99, 17);
-            this.overallButton.TabIndex = 0;
-            this.overallButton.TabStop = true;
-            this.overallButton.Text = "Overall Rank";
-            this.overallButton.UseVisualStyleBackColor = true;
-            // 
-            // timeButton
-            // 
-            this.timeButton.AutoSize = true;
-            this.timeButton.Location = new System.Drawing.Point(18, 62);
-            this.timeButton.Name = "timeButton";
-            this.timeButton.Size = new System.Drawing.Size(52, 17);
-            this.timeButton.TabIndex = 1;
-            this.timeButton.TabStop = true;
-            this.timeButton.Text = "Time";
-            this.timeButton.UseVisualStyleBackColor = true;
-            // 
-            // scoreButton
-            // 
-            this.scoreButton.AutoSize = true;
-            this.scoreButton.Location = new System.Drawing.Point(18, 97);
-            this.scoreButton.Name = "scoreButton";
-            this.scoreButton.Size = new System.Drawing.Size(58, 17);
-            this.scoreButton.TabIndex = 2;
-            this.scoreButton.TabStop = true;
-            this.scoreButton.Text = "Score";
-            this.scoreButton.UseVisualStyleBackColor = true;
-            // 
-            // listView1
-            // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.Rank,
-            this.Username,
-            this.Value});
-            this.listView1.GridLines = true;
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(37, 125);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(461, 285);
-            this.listView1.TabIndex = 1;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // Rank
-            // 
-            this.Rank.Text = "Rank";
-            this.Rank.Width = 90;
-            // 
-            // Username
-            // 
-            this.Username.Text = "Username";
-            this.Username.Width = 200;
-            // 
-            // Value
-            // 
-            this.Value.Text = "Value";
-            this.Value.Width = 200;
+            this.leaderboard.GridLines = true;
+            this.leaderboard.HideSelection = false;
+            this.leaderboard.Location = new System.Drawing.Point(56, 192);
+            this.leaderboard.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.leaderboard.Name = "leaderboard";
+            this.leaderboard.Size = new System.Drawing.Size(690, 436);
+            this.leaderboard.TabIndex = 1;
+            this.leaderboard.UseCompatibleStateImageBehavior = false;
+            this.leaderboard.View = System.Windows.Forms.View.Details;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(79, 54);
+            this.label1.Location = new System.Drawing.Point(118, 83);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(381, 25);
+            this.label1.Size = new System.Drawing.Size(551, 37);
             this.label1.TabIndex = 3;
             this.label1.Text = "Leaderboard for Sons of the Forest";
             // 
@@ -144,19 +73,58 @@
             this.gameGroupBox.Controls.Add(this.gameRadioButton2);
             this.gameGroupBox.Controls.Add(this.gameRadioButton1);
             this.gameGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gameGroupBox.Location = new System.Drawing.Point(542, 253);
+            this.gameGroupBox.Location = new System.Drawing.Point(812, 272);
+            this.gameGroupBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.gameGroupBox.Name = "gameGroupBox";
-            this.gameGroupBox.Size = new System.Drawing.Size(200, 174);
+            this.gameGroupBox.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gameGroupBox.Size = new System.Drawing.Size(300, 268);
             this.gameGroupBox.TabIndex = 3;
             this.gameGroupBox.TabStop = false;
             this.gameGroupBox.Text = "Game";
             // 
+            // gameRadioButton6
+            // 
+            this.gameRadioButton6.AutoSize = true;
+            this.gameRadioButton6.Location = new System.Drawing.Point(27, 215);
+            this.gameRadioButton6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gameRadioButton6.Name = "gameRadioButton6";
+            this.gameRadioButton6.Size = new System.Drawing.Size(98, 24);
+            this.gameRadioButton6.TabIndex = 5;
+            this.gameRadioButton6.TabStop = true;
+            this.gameRadioButton6.Text = "VRChat";
+            this.gameRadioButton6.UseVisualStyleBackColor = true;
+            // 
+            // gameRadioButton5
+            // 
+            this.gameRadioButton5.AutoSize = true;
+            this.gameRadioButton5.Location = new System.Drawing.Point(27, 180);
+            this.gameRadioButton5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gameRadioButton5.Name = "gameRadioButton5";
+            this.gameRadioButton5.Size = new System.Drawing.Size(91, 24);
+            this.gameRadioButton5.TabIndex = 4;
+            this.gameRadioButton5.TabStop = true;
+            this.gameRadioButton5.Text = "Roblox";
+            this.gameRadioButton5.UseVisualStyleBackColor = true;
+            // 
+            // gameRadioButton4
+            // 
+            this.gameRadioButton4.AutoSize = true;
+            this.gameRadioButton4.Location = new System.Drawing.Point(27, 145);
+            this.gameRadioButton4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gameRadioButton4.Name = "gameRadioButton4";
+            this.gameRadioButton4.Size = new System.Drawing.Size(156, 24);
+            this.gameRadioButton4.TabIndex = 3;
+            this.gameRadioButton4.TabStop = true;
+            this.gameRadioButton4.Text = "Counter-Strike";
+            this.gameRadioButton4.UseVisualStyleBackColor = true;
+            // 
             // gameRadioButton3
             // 
             this.gameRadioButton3.AutoSize = true;
-            this.gameRadioButton3.Location = new System.Drawing.Point(18, 71);
+            this.gameRadioButton3.Location = new System.Drawing.Point(27, 109);
+            this.gameRadioButton3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.gameRadioButton3.Name = "gameRadioButton3";
-            this.gameRadioButton3.Size = new System.Drawing.Size(72, 17);
+            this.gameRadioButton3.Size = new System.Drawing.Size(104, 24);
             this.gameRadioButton3.TabIndex = 2;
             this.gameRadioButton3.TabStop = true;
             this.gameRadioButton3.Text = "Valorant";
@@ -165,9 +133,10 @@
             // gameRadioButton2
             // 
             this.gameRadioButton2.AutoSize = true;
-            this.gameRadioButton2.Location = new System.Drawing.Point(18, 48);
+            this.gameRadioButton2.Location = new System.Drawing.Point(27, 74);
+            this.gameRadioButton2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.gameRadioButton2.Name = "gameRadioButton2";
-            this.gameRadioButton2.Size = new System.Drawing.Size(131, 17);
+            this.gameRadioButton2.Size = new System.Drawing.Size(190, 24);
             this.gameRadioButton2.TabIndex = 1;
             this.gameRadioButton2.TabStop = true;
             this.gameRadioButton2.Text = "Rainbow Six Seige";
@@ -176,71 +145,38 @@
             // gameRadioButton1
             // 
             this.gameRadioButton1.AutoSize = true;
-            this.gameRadioButton1.Location = new System.Drawing.Point(18, 25);
+            this.gameRadioButton1.Location = new System.Drawing.Point(27, 38);
+            this.gameRadioButton1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.gameRadioButton1.Name = "gameRadioButton1";
-            this.gameRadioButton1.Size = new System.Drawing.Size(129, 17);
+            this.gameRadioButton1.Size = new System.Drawing.Size(190, 24);
             this.gameRadioButton1.TabIndex = 0;
             this.gameRadioButton1.TabStop = true;
             this.gameRadioButton1.Text = "Sons of the Forest";
             this.gameRadioButton1.UseVisualStyleBackColor = true;
             // 
-            // gameRadioButton4
-            // 
-            this.gameRadioButton4.AutoSize = true;
-            this.gameRadioButton4.Location = new System.Drawing.Point(18, 94);
-            this.gameRadioButton4.Name = "gameRadioButton4";
-            this.gameRadioButton4.Size = new System.Drawing.Size(106, 17);
-            this.gameRadioButton4.TabIndex = 3;
-            this.gameRadioButton4.TabStop = true;
-            this.gameRadioButton4.Text = "Counter-Strike";
-            this.gameRadioButton4.UseVisualStyleBackColor = true;
-            // 
-            // gameRadioButton5
-            // 
-            this.gameRadioButton5.AutoSize = true;
-            this.gameRadioButton5.Location = new System.Drawing.Point(18, 117);
-            this.gameRadioButton5.Name = "gameRadioButton5";
-            this.gameRadioButton5.Size = new System.Drawing.Size(64, 17);
-            this.gameRadioButton5.TabIndex = 4;
-            this.gameRadioButton5.TabStop = true;
-            this.gameRadioButton5.Text = "Roblox";
-            this.gameRadioButton5.UseVisualStyleBackColor = true;
-            // 
-            // gameRadioButton6
-            // 
-            this.gameRadioButton6.AutoSize = true;
-            this.gameRadioButton6.Location = new System.Drawing.Point(18, 140);
-            this.gameRadioButton6.Name = "gameRadioButton6";
-            this.gameRadioButton6.Size = new System.Drawing.Size(68, 17);
-            this.gameRadioButton6.TabIndex = 5;
-            this.gameRadioButton6.TabStop = true;
-            this.gameRadioButton6.Text = "VRChat";
-            this.gameRadioButton6.UseVisualStyleBackColor = true;
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(176, 91);
+            this.label2.Location = new System.Drawing.Point(264, 140);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(163, 18);
+            this.label2.Size = new System.Drawing.Size(235, 29);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Sorting by Overall Rank";
+            this.label2.Text = "Sorting by Play Time";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.gameGroupBox);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.leaderboard);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.gameGroupBox.ResumeLayout(false);
             this.gameGroupBox.PerformLayout();
             this.ResumeLayout(false);
@@ -249,15 +185,7 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton scoreButton;
-        private System.Windows.Forms.RadioButton timeButton;
-        private System.Windows.Forms.RadioButton overallButton;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ColumnHeader Rank;
-        private System.Windows.Forms.ColumnHeader Username;
-        private System.Windows.Forms.ColumnHeader Value;
+        private System.Windows.Forms.ListView leaderboard;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox gameGroupBox;
         private System.Windows.Forms.RadioButton gameRadioButton3;
--- Leaderboard/Form1.cs
+++ Leaderboard/Form1.cs
@@ -13,9 +13,143 @@ namespace Leaderboard
 {
     public partial class Form1 : Form
     {
+        private bool isShuffled = false;
+        private bool isShuffled2 = false;
+        private bool isShuffled3 = false;
+        private bool isShuffled4 = false;
+        private bool isShuffled5 = false;
+        private bool isShuffled6 = false;
+
         public Form1()
         {
             InitializeComponent();
+
+            //game radio buttons
+            this.gameRadioButton1.CheckedChanged += new EventHandler(this.gameRadioButton1__CheckedChanged);
+            this.gameRadioButton2.CheckedChanged += new EventHandler(this.gameRadioButton2__CheckedChanged);
+            this.gameRadioButton3.CheckedChanged += new EventHandler(this.gameRadioButton3__CheckedChanged);
+            this.gameRadioButton4.CheckedChanged += new EventHandler(this.gameRadioButton4__CheckedChanged);
+            this.gameRadioButton5.CheckedChanged += new EventHandler(this.gameRadioButton5__CheckedChanged);
+            this.gameRadioButton6.CheckedChanged += new EventHandler(this.gameRadioButton6__CheckedChanged);
+
+
+            leaderboard.View = View.Details;
+            leaderboard.Columns.Add("Rank", 50); 
+            leaderboard.Columns.Add("Username", 250); 
+            leaderboard.Columns.Add("Playtime", 150); 
+
+            string[] usernames = { "ses011", "kash_registerr", "catas0phi", "goooober", "anonygoose", ".grbe", "not_phoeniix" };
+            Random random = new Random();
+
+            for (int i = 0; i < usernames.Length; i++)
+            {
+                ListViewItem item = new ListViewItem((i + 1).ToString());
+                item.SubItems.Add(usernames[i]);
+
+                TimeSpan randomTime = new TimeSpan(0, random.Next(0, 60), random.Next(0, 60), random.Next(0, 60));
+                item.SubItems.Add(randomTime.ToString(@"hh\:mm\:ss"));
+
+                leaderboard.Items.Add(item);
+            }
+
+
         }
+
+        //code to randomly shuffle the usernames in the leaderboard
+        private void ShuffleUsernames()
+        {
+            Random random = new Random();
+            List<ListViewItem> shuffledItems = new List<ListViewItem>(leaderboard.Items.Cast<ListViewItem>());
+
+            int n = shuffledItems.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+
+                string tempUsername = shuffledItems[k].SubItems[1].Text;
+                shuffledItems[k].SubItems[1].Text = shuffledItems[n].SubItems[1].Text;
+                shuffledItems[n].SubItems[1].Text = tempUsername;
+
+                //create random timespans using hours, minutes, and seconds to represent playtime
+                TimeSpan randomTimeK = new TimeSpan(0, random.Next(0, 60), random.Next(0, 60), random.Next(0, 60));
+                shuffledItems[k].SubItems[2].Text = randomTimeK.ToString(@"hh\:mm\:ss");
+
+                TimeSpan randomTimeN = new TimeSpan(0, random.Next(0, 60), random.Next(0, 60), random.Next(0, 60));
+                shuffledItems[n].SubItems[2].Text = randomTimeN.ToString(@"hh\:mm\:ss");
+            }
+
+            //clear the itms for new items
+            leaderboard.Items.Clear();
+            leaderboard.Items.AddRange(shuffledItems.ToArray());
+        }
+
+
+
+        private void gameRadioButton1__CheckedChanged(object sender, EventArgs e)
+        {
+            if (!isShuffled)
+            {
+                ShuffleUsernames();
+            }
+
+            this.label1.Text = "Leaderboard for Sons of the Forest";
+        }
+
+        private void gameRadioButton2__CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (!isShuffled2)
+            {
+                ShuffleUsernames();
+
+            }
+            this.label1.Text = "Leaderboard for Rainbow Six Seige";
+        }
+
+        private void gameRadioButton3__CheckedChanged(object sender, EventArgs e)
+        {
+            if (!isShuffled3)
+            {
+                ShuffleUsernames();
+
+            }
+
+            this.label1.Text = "Leaderboard for Valorant";
+        }
+
+        private void gameRadioButton4__CheckedChanged(object sender, EventArgs e)
+        {
+            if (!isShuffled4)
+            {
+                ShuffleUsernames();
+
+            }
+
+            this.label1.Text = "Leaderboard for Counter-Strike";
+        }
+
+        private void gameRadioButton5__CheckedChanged(object sender, EventArgs e)
+        {
+            if (!isShuffled5)
+            {
+                ShuffleUsernames();
+
+            }
+
+            this.label1.Text = "Leaderboard for Roblox";
+        }
+
+        private void gameRadioButton6__CheckedChanged(object sender, EventArgs e)
+        {
+            if (!isShuffled6)
+            {
+                ShuffleUsernames();
+
+            }
+
+            this.label1.Text = "Leaderboard for VRChat";
+        }
+
     }
 }

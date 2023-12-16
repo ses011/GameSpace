62eb871f9261e2dfa28b2cbb68c249948a3b0b61
Sarah Schneider <sses7344@gmail.com>
2023-12-16 17:38:14 -0500

    stuff

--- HomepagePrototype/Form1.Designer.cs
+++ HomepagePrototype/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
             this.toolStripLabel4 = new System.Windows.Forms.ToolStripLabel();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.matchButton = new System.Windows.Forms.Button();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -50,13 +50,14 @@
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripButton1,
             this.toolStripLabel1,
-            this.toolStripLabel2,
             this.toolStripLabel3,
+            this.toolStripLabel2,
             this.toolStripButton2,
             this.toolStripLabel4});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(533, 31);
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 3, 0);
+            this.toolStrip1.Size = new System.Drawing.Size(800, 37);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -66,90 +67,91 @@
             this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
             this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(28, 28);
+            this.toolStripButton1.Size = new System.Drawing.Size(34, 32);
             this.toolStripButton1.Text = "toolStripButton1";
             // 
             // toolStripLabel1
             // 
+            this.toolStripLabel1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.toolStripLabel1.Margin = new System.Windows.Forms.Padding(100, 2, 0, 3);
             this.toolStripLabel1.Name = "toolStripLabel1";
-            this.toolStripLabel1.Size = new System.Drawing.Size(58, 26);
+            this.toolStripLabel1.Size = new System.Drawing.Size(88, 32);
             this.toolStripLabel1.Text = "Match!";
             // 
             // toolStripLabel2
             // 
+            this.toolStripLabel2.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.toolStripLabel2.Margin = new System.Windows.Forms.Padding(75, 2, 0, 3);
             this.toolStripLabel2.Name = "toolStripLabel2";
-            this.toolStripLabel2.Size = new System.Drawing.Size(42, 26);
+            this.toolStripLabel2.Size = new System.Drawing.Size(63, 32);
             this.toolStripLabel2.Text = "Chat";
             // 
             // toolStripLabel3
             // 
+            this.toolStripLabel3.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.toolStripLabel3.Margin = new System.Windows.Forms.Padding(75, 2, 0, 3);
             this.toolStripLabel3.Name = "toolStripLabel3";
-            this.toolStripLabel3.Size = new System.Drawing.Size(105, 26);
+            this.toolStripLabel3.Size = new System.Drawing.Size(157, 32);
             this.toolStripLabel3.Text = "Leaderboards";
             // 
             // toolStripButton2
             // 
             this.toolStripButton2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
             this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
             this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(28, 34);
+            this.toolStripButton2.Size = new System.Drawing.Size(34, 32);
             this.toolStripButton2.Text = "toolStripButton2";
             // 
             // toolStripLabel4
             // 
             this.toolStripLabel4.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            this.toolStripLabel4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripLabel4.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.toolStripLabel4.Name = "toolStripLabel4";
-            this.toolStripLabel4.Size = new System.Drawing.Size(69, 34);
+            this.toolStripLabel4.Size = new System.Drawing.Size(119, 32);
             this.toolStripLabel4.Text = "username";
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(105, 111);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(158, 171);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(189, 35);
+            this.label1.Size = new System.Drawing.Size(284, 54);
             this.label1.TabIndex = 0;
             this.label1.Text = "Get started here";
             // 
-            // button1
+            // matchButton
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(69, 148);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(261, 66);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "MATCH!";
-            this.button1.UseVisualStyleBackColor = false;
+            this.matchButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.matchButton.Font = new System.Drawing.Font("Yu Gothic UI", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.matchButton.Location = new System.Drawing.Point(104, 228);
+            this.matchButton.Name = "matchButton";
+            this.matchButton.Size = new System.Drawing.Size(392, 102);
+            this.matchButton.TabIndex = 1;
+            this.matchButton.Text = "MATCH!";
+            this.matchButton.UseVisualStyleBackColor = false;
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(381, 25);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(572, 38);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(153, 268);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(230, 412);
             this.flowLayoutPanel1.TabIndex = 2;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 449);
             this.Controls.Add(this.flowLayoutPanel1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.matchButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.toolStrip1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "GameSpace";
             this.toolStrip1.ResumeLayout(false);
@@ -169,7 +171,7 @@
         private System.Windows.Forms.ToolStripButton toolStripButton2;
         private System.Windows.Forms.ToolStripLabel toolStripLabel4;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button matchButton;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
--- HomepagePrototype/Form1.cs
+++ HomepagePrototype/Form1.cs
@@ -8,13 +8,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using PlayerInfo;
+
 namespace HomepagePrototype
 {
+
+    public static void Main()
+    {
+        PlayerInfo players = new PlayerInfo();
+        userList.Add("ses011", new User("ses011", "nope. just nope", false));
+        userList.Add("kash_registerr", new User("kash_registerr", "", false));
+        userList.Add("catast0phi", new User("catast0phi", "oh boy!!! VIOLENCE for christmas!!!!!!", false));
+        userList.Add("goooobr", new User("gooobr", ":3", false));
+        userList.Add("anonygoose", new User("anonygoose", "your local fisherman dad", false));
+        userList.Add(".grbe", new User(".grbe", "", false));
+        userList.Add("not_phoeniix", new User("not_phoeniix", "they call me the Grinch\nthey call me\nthey call me the\nthey\nGrinch", false));
+    }
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+
+
+            this.matchButton.Click += new EventHandler(MatchButton__Click);
+        }
+
+        public void MatchButton__Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }

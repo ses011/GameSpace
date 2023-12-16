858eae2936b50e806456b82fdc8d6fe5a15924f4
Sarah Schneider <sses7344@gmail.com>
2023-12-16 17:44:22 -0500

    profile to DLL

--- HomepagePrototype/Form1.cs
+++ HomepagePrototype/Form1.cs
@@ -12,18 +12,6 @@ using PlayerInfo;
 
 namespace HomepagePrototype
 {
-
-    public static void Main()
-    {
-        PlayerInfo players = new PlayerInfo();
-        userList.Add("ses011", new User("ses011", "nope. just nope", false));
-        userList.Add("kash_registerr", new User("kash_registerr", "", false));
-        userList.Add("catast0phi", new User("catast0phi", "oh boy!!! VIOLENCE for christmas!!!!!!", false));
-        userList.Add("goooobr", new User("gooobr", ":3", false));
-        userList.Add("anonygoose", new User("anonygoose", "your local fisherman dad", false));
-        userList.Add(".grbe", new User(".grbe", "", false));
-        userList.Add("not_phoeniix", new User("not_phoeniix", "they call me the Grinch\nthey call me\nthey call me the\nthey\nGrinch", false));
-    }
     public partial class Form1 : Form
     {
         public Form1()

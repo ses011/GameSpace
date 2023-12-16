c4efd41732549fc80049466035e666e7a1bdfd5a
Sarah Schneider <sses7344@gmail.com>
2023-12-16 17:48:38 -0500

    Moved main

--- HomepagePrototype/Program.cs
+++ HomepagePrototype/Program.cs
@@ -15,14 +15,13 @@ namespace HomepagePrototype
         [STAThread]
         static void Main()
         {
-            PlayerInfo players = new PlayerInfo();
-            userList.Add("ses011", new User("ses011", "nope. just nope", false));
-            userList.Add("kash_registerr", new User("kash_registerr", "", false));
-            userList.Add("catast0phi", new User("catast0phi", "oh boy!!! VIOLENCE for christmas!!!!!!", false));
-            userList.Add("goooobr", new User("gooobr", ":3", false));
-            userList.Add("anonygoose", new User("anonygoose", "your local fisherman dad", false));
-            userList.Add(".grbe", new User(".grbe", "", false));
-            userList.Add("not_phoeniix", new User("not_phoeniix", "they call me the Grinch\nthey call me\nthey call me the\nthey\nGrinch", false));
+            Players.userList.Add("ses011", new User("ses011", "nope. just nope", false));
+            Players.userList.Add("kash_registerr", new User("kash_registerr", "", false));
+            Players.userList.Add("catast0phi", new User("catast0phi", "oh boy!!! VIOLENCE for christmas!!!!!!", false));
+            Players.userList.Add("goooobr", new User("gooobr", ":3", false));
+            Players.userList.Add("anonygoose", new User("anonygoose", "your local fisherman dad", false));
+            Players.userList.Add(".grbe", new User(".grbe", "", false));
+            Players.userList.Add("not_phoeniix", new User("not_phoeniix", "they call me the Grinch\nthey call me\nthey call me the\nthey\nGrinch", false));
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);

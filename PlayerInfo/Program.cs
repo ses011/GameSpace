f594d0d19d4855674a037f3a99617c26834e6961
Kash <kasha@DESKTOP-TKSG659>
2023-12-17 13:16:10 -0500

    Edited database

--- PlayerInfo/Program.cs
+++ PlayerInfo/Program.cs
@@ -8,16 +8,12 @@ using PlayerInfo;
 using GameChat;
 using System.Runtime.Remoting.Messaging;
 
-/**
- * Authors: Kashaf Ahmed (majority) and Sarah Schnedier
- * 
- * 
- * 
+/*
+ * Authors: Kashaf Ahmed and Sarah Schnedier
  */
 namespace PlayerInfo
 {
-    //we could also user enumerated type to store data from sorted lists instead (we can't add spaces to enumerated types so
-    //maybe sorted list would be better for the things that have spaces in between maybe?
+    //Enumerated type to hold the game names
     public enum EGames 
     {
       Sons_of_The_Forest,
@@ -28,8 +24,15 @@ namespace PlayerInfo
       VRChat
     
     }
-    
-   
+
+
+    // Class: User
+    // Author: Kashaf Ahmed and Sarah Schnedier
+    // Purpose: Creates the user object with a username, bio, online status, profile picture, as well as 
+    // the lists for the games, times, rank, and friends for the current user. The sorted lists are populated
+    // from different methods that Sarah had written. The time and rank are accessed by the game name (key) and everything else
+    // is accessed from the username (key).
+    // Restrictions: None
     public class User
     {
 
@@ -38,14 +41,13 @@ namespace PlayerInfo
         public bool onlineStatus;
         public string pfp;
 
-        //sorted list that takes in the username as the key and the games as a value (could use a tuple for the value to store multiple values in)
-        //of if there was an easier way of doing it not too sure 
+        
         public SortedList<string, string> gameNames = new SortedList<string, string>();
-        //username is key and the time is the value (could also use the tuple approach for the value so we could store multiple things in one value)
+      
         public SortedList<string, double> times = new SortedList<string, double>();
-        //username is key and the rank is the value(could also use the tuple approach for the value so we could store multiple things in one value)
+      
         public SortedList<string, int> rank = new SortedList<string, int>();
-        //a user's friends
+       
         public SortedList<string, string> friends = new SortedList<string, string>();
 
 
@@ -105,44 +107,16 @@ namespace PlayerInfo
             return rank;
         }
 
-        //implement each list in UserDataBase file we will create and populate the userList defined in Players based on the username as the key
-        //and the User object as the value 
-
-
-        //was not too sure if we wanted a comparison here either but kept it just in case 
-        //public static bool operator <(Student s1, Student s2)
-        //{
-        //    return (s1.gpa < s2.gpa);
-        //}
-        //
-        //public static bool operator <=(Student s1, Student s2)
-        //{
-        //    return (s1.gpa <= s2.gpa);
-        //}
-        //
-        //public static bool operator >(Student s1, Student s2)
-        //{
-        //    return (s1.gpa > s2.gpa);
-        //}
-        //
-        //public static bool operator >=(Student s1, Student s2)
-        //{
-        //    return (s1.gpa >= s2.gpa);
-        //}
-        //
-        //public static bool operator ==(Student s1, Student s2)
-        //{
-        //    return (s1.gpa == s2.gpa);
-        //}
-        //
-        //public static bool operator !=(Student s1, Student s2)
-        //{
-        //    return (s1.gpa != s2.gpa);
-        //}
+       
     }
 
-    // [+People|sortedList:SortedList<string, Person>|this:email|+Remove(email: string)]
-    public class Players //did players here instead of users so it would not be confusing 
+
+    // Class: Players
+    // Author: Kashaf Ahmed 
+    // Purpose: Create the list of users and have the username be the key to remove a user object from the list if necessary.
+    // the indexer property allows us to access the sorted list and get a user object based on the username.
+    // Restrictions: None
+    public class Players 
     {
 
         // create a Sorted List indexed on username (string) and storing User objects
@@ -186,7 +160,7 @@ namespace PlayerInfo
                 catch
                 {
                     // an exception will be raised if an entry with a duplicate key is added
-                    // duplicate key handling
+                    // duplicate key handling (should we write something for this catch statement or is it ok to leave it empty)**
                 }
             }
         }

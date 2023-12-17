b7d2142ff71806977a3be9b75f1230439071fc16
Kash <kasha@DESKTOP-TKSG659>
2023-12-17 17:10:52 -0500

    Edited

--- Profile-Display/Form1.cs
+++ Profile-Display/Form1.cs
@@ -63,7 +63,7 @@ namespace Profile_Display
         private void DmButton__Click(object sender, EventArgs e)
         {
             this.Enabled = false;
-            new ChatForm(formUser, formUser.username); 
+            new ChatForm(formUser); 
         }
 
 

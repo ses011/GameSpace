b196a5a8f66e7e73d2d32bdd5aafec1b3fa12c5d
Sarah Schneider <sses7344@gmail.com>
2023-12-17 16:22:46 -0500

    images

--- HomepagePrototype/Program.cs
+++ HomepagePrototype/Program.cs
@@ -15,7 +15,7 @@ namespace HomepagePrototype
         [STAThread]
         static void Main()
         {
-            Players.userList.Add("ses011", new User("ses011", "nope. just nope", false, ""));
+            Players.userList.Add("ses011", new User("ses011", "nope. just nope", false, "https://i.pinimg.com/564x/ef/3e/e1/ef3ee14feee15daddd90a0751597c97f.jpg"));
             Players.userList.Add("kash_registerr", new User("kash_registerr", "", false, "https://i.pinimg.com/originals/04/92/09/049209be806b75a21290058ea12160e7.jpg"));
             Players.userList.Add("catast0phi", new User("catast0phi", "oh boy!!! VIOLENCE for christmas!!!!!!", false, ""));
             Players.userList.Add("goooobr", new User("gooobr", ":3", false, ""));

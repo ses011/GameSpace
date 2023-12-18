d5b61b5ee4b9957e059c05f6178834966d48b9fa
Joey Reidy <email4joeyreidy@gmail.com>
2023-12-18 01:19:53 -0500

    push

--- GameChat/Form1.Designer.cs
+++ GameChat/Form1.Designer.cs
@@ -44,6 +44,7 @@
             this.robloxButton = new System.Windows.Forms.Button();
             this.vrChatButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.vrChatPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.robloxPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.counterStrikePictureBox)).BeginInit();
@@ -209,11 +210,21 @@
             this.label2.TabIndex = 14;
             this.label2.Text = "Meetups";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(678, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // GameChatForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.vrChatButton);
             this.Controls.Add(this.robloxButton);
@@ -259,6 +270,7 @@
         private System.Windows.Forms.Button robloxButton;
         private System.Windows.Forms.Button vrChatButton;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 

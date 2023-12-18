4ada3a9ba74899d6c0be2e37face168f2bb5ac29
B3PIS <B3PIS@DESKTOP-R1BQA74>
2023-12-18 18:20:49 -0500

    matchup fix

--- Matchups/Form1.cs
+++ Matchups/Form1.cs
@@ -10,15 +10,18 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Collections;
 using System.CodeDom.Compiler;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace Matchups
 {
     public partial class MatchupForm : Form
     {
+        int currentUser = 0;
+        IList<string> names;
+
         public MatchupForm()
         {
             InitializeComponent();
-
 
             int[] compat = new int[Players.userList.Count];
             for (int i = 0; i < compat.Length; i++)
@@ -26,20 +29,9 @@ namespace Matchups
                 compat[i] = 0;
             }
 
+            names = Players.userList.Keys;
 
-            User thisUser = Players.userList["ses011"];
-            List<string> gamesList = thisUser.gameNames;
-
-
-            game1.Text = "Game 1";
-            game2.Text = "Game 2";
-            game3.Text = "Game 3";
-
-            nameLabel.Text = thisUser.username;
-
-            compatLabel.Text = "%%";
-
-            pictureBox1.ImageLocation = thisUser.pfp;
+            UpdateProfile();
 
             //this.FormClosing += new FormClosingEventHandler(Form__Closing);
 
@@ -50,6 +42,34 @@ namespace Matchups
             MatchupForm form = new MatchupForm();
         }*/
 
+        private void UpdateProfile()
+        {
+            User thisUser = Players.userList[names[currentUser]];
+            List<string> gamesList = thisUser.gameNames;
+
+            for (int i = 0; i < thisUser.gameNames.Count; i++)
+            {
+                switch (i)
+                {
+                    case (0):
+                        game1.Text = gamesList[0];
+                        break;
+                    case (1):
+                        game2.Text = gamesList[1];
+                        break;
+                    case (2):
+                        game3.Text = gamesList[2];
+                        break;
+                }
+            }
+
+            nameLabel.Text = thisUser.username;
+
+            compatLabel.Text = "%%";
+
+            pictureBox1.ImageLocation = thisUser.pfp;
+        }
+
         private void homeLabel_Click(object sender, EventArgs e)
         {
 
@@ -57,11 +77,21 @@ namespace Matchups
 
         private void nextButton_Click(object sender, EventArgs e)
         {
+            if(currentUser < names.Count - 1)
+            {
+                currentUser++;
+                UpdateProfile();
+            }
 
         }
 
         private void previousButton_Click(object sender, EventArgs e)
         {
+            if (currentUser > 0)
+            {
+                currentUser--;
+                UpdateProfile();
+            }
 
         }
 
@@ -71,6 +101,16 @@ namespace Matchups
         }
 
         private void MatchupForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void compatLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nameLabel_Click(object sender, EventArgs e)
         {
 
         }

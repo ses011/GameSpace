928d02864bb792d6309d047786c7a1bb92acec28
Joey Reidy <email4joeyreidy@gmail.com>
2023-12-20 02:11:04 -0500

    meetup

--- GameChat/Meetup.Designer.cs
+++ GameChat/Meetup.Designer.cs
@@ -40,34 +40,36 @@
             // nameLabel
             // 
             this.nameLabel.AutoSize = true;
-            this.nameLabel.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nameLabel.Location = new System.Drawing.Point(213, 12);
+            this.nameLabel.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.nameLabel.Location = new System.Drawing.Point(185, 9);
             this.nameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(64, 30);
+            this.nameLabel.Size = new System.Drawing.Size(74, 32);
             this.nameLabel.TabIndex = 0;
             this.nameLabel.Text = "event";
-            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // peopleLabel
             // 
             this.peopleLabel.AutoSize = true;
-            this.peopleLabel.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.peopleLabel.Location = new System.Drawing.Point(187, 246);
+            this.peopleLabel.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.peopleLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.peopleLabel.Location = new System.Drawing.Point(187, 225);
             this.peopleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.peopleLabel.Name = "peopleLabel";
-            this.peopleLabel.Size = new System.Drawing.Size(111, 17);
+            this.peopleLabel.Size = new System.Drawing.Size(132, 21);
             this.peopleLabel.TabIndex = 2;
             this.peopleLabel.Text = "People Attending:";
             // 
             // locationLabel
             // 
             this.locationLabel.AutoSize = true;
-            this.locationLabel.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.locationLabel.Location = new System.Drawing.Point(213, 160);
+            this.locationLabel.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.locationLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.locationLabel.Location = new System.Drawing.Point(187, 153);
             this.locationLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.locationLabel.Name = "locationLabel";
-            this.locationLabel.Size = new System.Drawing.Size(54, 17);
+            this.locationLabel.Size = new System.Drawing.Size(65, 21);
             this.locationLabel.TabIndex = 4;
             this.locationLabel.Text = "location";
             this.locationLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -75,11 +77,12 @@
             // interestCheckBox
             // 
             this.interestCheckBox.AutoSize = true;
-            this.interestCheckBox.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.interestCheckBox.Location = new System.Drawing.Point(200, 370);
-            this.interestCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.interestCheckBox.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.interestCheckBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.interestCheckBox.Location = new System.Drawing.Point(187, 348);
+            this.interestCheckBox.Margin = new System.Windows.Forms.Padding(4);
             this.interestCheckBox.Name = "interestCheckBox";
-            this.interestCheckBox.Size = new System.Drawing.Size(106, 21);
+            this.interestCheckBox.Size = new System.Drawing.Size(124, 25);
             this.interestCheckBox.TabIndex = 5;
             this.interestCheckBox.Text = "I\'m Interested";
             this.interestCheckBox.UseVisualStyleBackColor = true;
@@ -87,30 +90,33 @@
             // timeLabel
             // 
             this.timeLabel.AutoSize = true;
-            this.timeLabel.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timeLabel.Location = new System.Drawing.Point(213, 185);
+            this.timeLabel.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.timeLabel.Location = new System.Drawing.Point(187, 183);
             this.timeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.timeLabel.Name = "timeLabel";
-            this.timeLabel.Size = new System.Drawing.Size(33, 17);
+            this.timeLabel.Size = new System.Drawing.Size(41, 21);
             this.timeLabel.TabIndex = 6;
             this.timeLabel.Text = "time";
             this.timeLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // attendingTextBox
             // 
-            this.attendingTextBox.Location = new System.Drawing.Point(187, 271);
-            this.attendingTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.attendingTextBox.Location = new System.Drawing.Point(187, 250);
+            this.attendingTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.attendingTextBox.Multiline = true;
             this.attendingTextBox.Name = "attendingTextBox";
+            this.attendingTextBox.ReadOnly = true;
             this.attendingTextBox.Size = new System.Drawing.Size(135, 78);
             this.attendingTextBox.TabIndex = 7;
             // 
             // descriptionTextBox
             // 
             this.descriptionTextBox.Location = new System.Drawing.Point(187, 49);
-            this.descriptionTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.descriptionTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.descriptionTextBox.Multiline = true;
             this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.ReadOnly = true;
             this.descriptionTextBox.Size = new System.Drawing.Size(135, 100);
             this.descriptionTextBox.TabIndex = 8;
             // 
@@ -118,6 +124,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
             this.ClientSize = new System.Drawing.Size(541, 421);
             this.Controls.Add(this.descriptionTextBox);
             this.Controls.Add(this.attendingTextBox);
@@ -127,7 +134,7 @@
             this.Controls.Add(this.peopleLabel);
             this.Controls.Add(this.nameLabel);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Meetup";
             this.Text = "Meetup";
             this.ResumeLayout(false);
